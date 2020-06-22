1a20caa74084b96fa5ec0b3661db60c83899ca1a
Multithread <spiele@hundestammbaum.ch>
2020-06-22 20:29:23 +0200

    Field Access Check

---
+++ CodeConverterCore/Helper/Create.cs
@@ -0,0 +1,55 @@
+﻿using CodeConverterCore.Interface;
+using CodeConverterCore.Model;
+using System.Linq;
+using System.Reflection;
+
+namespace CodeConverterCore.Helper
+{
+    public static class Create
+    {
+        /// <summary>
+        /// Create ClassContainer
+        /// </summary>
+        public static ClassContainer AddClass(string inName, string inNamespace = null, params string[] inUsings)
+        {
+            return new ClassContainer
+            {
+                Type = new TypeContainer() { Name = inName, Type = new BaseType(inName) },
+                Namespace = inNamespace ?? ""
+            };
+        }
+
+        /// <summary>
+        /// Add Field to Class
+        /// </summary>
+        public static ClassContainer AddField(this ClassContainer inClass, string inFieldName, BaseType inType, string inDefaultValue = null, params FieldAttributes[] inAttributes)
+        {
+            inClass.FieldList.Add(new FieldContainer()
+            {
+                Name = inFieldName,
+                DefaultValue = inDefaultValue,
+                Type = new TypeContainer { Type = inType, Name = inType.Name }
+            });
+            return inClass;
+        }
+
+        /// <summary>
+        /// Add Methode to Class
+        /// </summary>
+        public static ClassContainer AddMethode(this ClassContainer inClass, string inMethodeName, BaseType inReturnType, params ICodeEntry[] inCode)
+        {
+            inClass.MethodeList.Add(new MethodeContainer()
+            {
+                Name = inMethodeName,
+                ReturnType = new TypeContainer { Type = inReturnType, Name = inReturnType.Name },
+                Code = new CodeBlock
+                {
+                    CodeEntries = inCode.ToList()
+                }
+            }); ;
+            return inClass;
+        }
+
+
+    }
+}
---
+++ JavaCodeLoader_Unittest/Objektstruktur/FieldAccess_Unittest.cs
@@ -0,0 +1,36 @@
+﻿using CodeConverterCore.Enum;
+using CodeConverterCore.Helper;
+using CodeConverterCore.Model;
+using CodeConverterJava.Model;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace CodeConverterJava_Unittest.Objektstruktur
+{
+    /// <summary>
+    /// Checking Field Write Access
+    /// </summary>
+    public class FieldAccess_Unittest
+    {
+        [Test]
+        public void MethodeVariableDeclaration()
+        {
+            var tmpClass = @"
+package org;
+public class Class1 {
+public int Value;
+public void Run(){
+Value=4;
+}
+}
+";
+            var tmpIniData = DataHelper.LoadIni("");
+            var tmpObjectInformation = new JavaLoader().CreateObjectInformation(new List<string> { tmpClass }, tmpIniData);
+
+            var tmpMethodeContent = tmpObjectInformation.ClassList[0].MethodeList[0];
+            var tmpCodeLine1 = (tmpMethodeContent.Code.CodeEntries[0] as StatementCode).StatementCodeBlocks[0].CodeEntries[0] as SetFieldWithValue;
+            Assert.AreEqual("Value", (tmpCodeLine1.VariableToAccess.CodeEntries[0] as ConstantValue).Value);
+            Assert.AreEqual("4", (tmpCodeLine1.ValueToSet.CodeEntries[0] as ConstantValue).Value);
+        }
+    }
+}
